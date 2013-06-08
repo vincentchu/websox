1ff91525c84feb85e5f65af02a95d521132d2459
Vincent Chu <vince@twitter.com>
2013-06-07 19:46:08 -0700

    Fixing indents

--- src/main/scala/com/vincentchu/websox/App.scala
+++ src/main/scala/com/vincentchu/websox/App.scala
@@ -8,34 +8,34 @@ object App {
   def main(args: Array[String]) {
     println("hello, world")
 
-  val service = new LocalWebsocketService[String] {
-    def onConnect(socketId: SocketId): Future[Unit] = {
-      println("FooService onConnect")
-      Future.Unit
-    }
+    val service = new LocalWebsocketService[String] {
+      def onConnect(socketId: SocketId): Future[Unit] = {
+        println("FooService onConnect")
+        Future.Unit
+      }
 
-    def onMessage(socketId: SocketId, msg: String): Future[Unit] = {
-      println("FooService onMessage received", msg, "from", socketId)
-      val mm: String = "You sez: " + msg
+      def onMessage(socketId: SocketId, msg: String): Future[Unit] = {
+        println("FooService onMessage received", msg, "from", socketId)
+        val mm: String = "You sez: " + msg
 
-      if (msg == "closeme") {
-        close(socketId)
-      } else {
-        writeMessage(socketId, mm)
+        if (msg == "closeme") {
+          close(socketId)
+        } else {
+          writeMessage(socketId, mm)
+        }
+      }
+
+      def onClose(socketId: SocketId): Future[Unit] = {
+        println("FooService onClose from", socketId)
+        Future.Unit
       }
     }
 
-    def onClose(socketId: SocketId): Future[Unit] = {
-      println("FooService onClose from", socketId)
-      Future.Unit
-    }
-  }
-
-  val config = ServerConfig(
-    StringMessage,
-    service,
-    8080
-  )
+    val config = ServerConfig(
+      StringMessage,
+      service,
+      8080
+    )
 
     Server(config).bind()
   }
