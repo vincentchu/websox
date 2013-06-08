6e3fe374def87067fbd0fd0a910b5faa48bbbd53
Vincent Chu <vince@twitter.com>
2013-06-07 20:28:23 -0700

    Fixing pom.xml spacing / comments

--- src/main/scala/com/vincentchu/websox/websocket/LocalWebsocketService.scala
+++ src/main/scala/com/vincentchu/websox/websocket/LocalWebsocketService.scala
@@ -7,13 +7,13 @@ trait LocalWebsocketService[A] extends WebsocketService[A] {
 
   import WebsocketService._
 
-  private[this] val contextMap = new ConcurrentHashMap[SocketId, Websocket[A]]()
+  private[this] val socketMap = new ConcurrentHashMap[SocketId, Websocket[A]]()
 
   protected[this] def get(socketId: SocketId): Option[Websocket[A]] =
-    Option(contextMap.get(socketId))
+    Option(socketMap.get(socketId))
 
   def registerSocket(ws: Websocket[A]): Future[Unit] = {
-    Option(contextMap.putIfAbsent(ws.socketId, ws)) match {
+    Option(socketMap.putIfAbsent(ws.socketId, ws)) match {
       case Some(_) => Future.exception(SocketIdExists)
       case None    => Future.Unit
     }
@@ -23,7 +23,7 @@ trait LocalWebsocketService[A] extends WebsocketService[A] {
     get(socketId) match {
       case None         => Future.exception(SocketIdNotFound)
       case Some(socket) => onClose(socketId) ensure {
-        contextMap.remove(socketId)
+        socketMap.remove(socketId)
         if (closeChannel) {
           socket.close()
         }
